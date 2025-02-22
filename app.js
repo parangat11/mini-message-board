b4f4c087c79c24fac99061318a031c9b6cabb53e
parangat11 <parangataditya001@gmail.com>
2025-02-23 02:42:10 +0530

    Change the port to account for the platform's available port

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.use("/new", newMessageRouter)
 app.use("/messages", messageRouter)
 app.use("/", indexRouter)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
